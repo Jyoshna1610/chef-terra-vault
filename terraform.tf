280473015dbc985bd5175e7be300a56edc8ea005
Jyoshna1610 <65143653+Jyoshna1610@users.noreply.github.com>
2021-08-19 17:38:01 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "web1" {
    ami           = "ami-0c2b8ca1dad447f8a"
    instance_type = "t2.micro"
    count = 1
-  vpc_security_group_ids = ["sg-01718400c2741bdb3"]
-   key_name               = "Linux_Terraform-Chef" 
+  vpc_security_group_ids = ["sg-0b37ee9a54bce1416"]
+    
    iam_instance_profile =   "myManagedInstanceRole"
    user_data = <<-EOF
 		#!/bin/bash
